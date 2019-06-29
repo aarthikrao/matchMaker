4fcefd87c61d4258a0d2845310df62f8f2b8d14a
Aarthik Rao <aaarthik@gmail.com>
2019-06-29 17:16:37 +0530

    Match making on rank

--- main.go
+++ main.go
@@ -2,15 +2,52 @@ package main
 
 import (
 	"fmt"
+	"sort"
 
 	"github.com/aarthikrao/matchMaker/models"
 	"github.com/aarthikrao/matchMaker/teams"
 )
 
 func main() {
+	// Create new members
+	listOfMembers := []models.Member{}
+	for i := 0; i < 6; i++ {
+		newMember := models.Member{
+			Name: fmt.Sprintf("player%d", i),
+			Rank: i,
+		}
+		listOfMembers = append(listOfMembers, newMember)
+	}
+	fmt.Println(listOfMembers)
+
+	allMatches := []models.Match{}
 	ch := make(chan models.EligibleTeams)
-	go teams.GetCombinations(ch, 6, 3)
-	for i := range ch {
-		fmt.Println(i)
+	go teams.GetCombinations(ch, len(listOfMembers), 2)
+	for eT := range ch {
+		newMatch := models.Match{}
+		teamAAvg := 0
+		for _, memberInTeamA := range eT.TeamA {
+			newMatch.TeamA.Members = append(newMatch.TeamA.Members, listOfMembers[memberInTeamA])
+			teamAAvg += listOfMembers[memberInTeamA].Rank
+		}
+		teamAAvg = teamAAvg / len(newMatch.TeamA.Members)
+		newMatch.TeamA.ARank = teamAAvg
+		teamBAvg := 0
+		for _, memberInTeamB := range eT.TeamB {
+			newMatch.TeamB.Members = append(newMatch.TeamB.Members, listOfMembers[memberInTeamB])
+			teamBAvg += listOfMembers[memberInTeamB].Rank
+		}
+		teamBAvg = teamBAvg / len(newMatch.TeamB.Members)
+		newMatch.TeamB.ARank = teamBAvg
+		allMatches = append(allMatches, newMatch)
+		fmt.Println(newMatch)
+	}
+	sort.Slice(allMatches, func(i, j int) bool {
+		return allMatches[i].TeamA.ARank-allMatches[i].TeamB.ARank > allMatches[j].TeamA.ARank-allMatches[j].TeamB.ARank
+	})
+	fmt.Println("######################################")
+	for _, match := range allMatches {
+		x := match.TeamA.ARank - match.TeamB.ARank
+		fmt.Println(x)
 	}
 }
--- models/models.go
+++ models/models.go
@@ -4,3 +4,18 @@ type EligibleTeams struct {
 	TeamA []int
 	TeamB []int
 }
+
+type Member struct {
+	Name string
+	Rank int
+}
+
+type Team struct {
+	Members []Member
+	ARank   int // Average rank of the team
+}
+
+type Match struct {
+	TeamA Team
+	TeamB Team
+}
